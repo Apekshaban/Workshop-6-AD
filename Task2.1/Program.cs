19412fd8fbfad14643c62a0e3d26e272149d8138
Apeksha-1-hub <meeraban123@gmail.com>
2025-12-03 17:21:45 +0545

    Task2.1 complete

--- Task2.1/Program.cs
+++ Task2.1/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace Task2._1
 {
+      
+    public delegate double DiscountStrategy(double price);
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double price = 1000;
+
+            // Calling using different discount strategies 
+            Console.WriteLine("Using Discount Strategy Methods");
+            Console.WriteLine($"Festival Discount (20%): {CalculateFinalPrice(price, FestivalDiscount)}");
+            Console.WriteLine($"Seasonal Discount (10%): {CalculateFinalPrice(price, SeasonalDiscount)}");
+            Console.WriteLine($"No Discount: {CalculateFinalPrice(price, NoDiscount)}");
+
+            // Calling using lambda 
+            Console.WriteLine("\nUsing Lambda Expression (30% Discount)");
+            Console.WriteLine($"30% Discount: {CalculateFinalPrice(price, p => p * 0.70)}");
+
+            Console.ReadLine();
         }
+
+
+        
+        public static double CalculateFinalPrice(double originalPrice, DiscountStrategy strategy)
+        {
+            return strategy(originalPrice);
+        }
+
+        
+        static double FestivalDiscount(double price) => price * 0.80;  // 20% off
+        static double SeasonalDiscount(double price) => price * 0.90;  // 10% off
+        static double NoDiscount(double price) => price;               // no discount
     }
 }

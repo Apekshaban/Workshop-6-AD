66c9fe557391f6160caeb31be937a9e40e009f66
Apeksha-1-hub <meeraban123@gmail.com>
2025-12-03 17:35:30 +0545

    Task5 complete

---
+++ Task5/Applicant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Applicant
+    {
+        // Applicant Name, Age Property
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Task5/CashierSales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class CashierSales
+    {
+        // Cashier ID, Sale Amount Property
+        public int CashierId { get; set; }
+        public decimal SaleAmount { get; set; }
+    }
+}

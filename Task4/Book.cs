455959e9f138ee973c9a1d27ede316c0cc135570
Apeksha-1-hub <meeraban123@gmail.com>
2025-12-03 17:30:42 +0545

    Task4 complete

---
+++ Task4/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Book
+    {
+        // Book Title, Price Property
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+}
